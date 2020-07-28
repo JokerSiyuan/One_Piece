46ff03060ead7591c625749507b3630e649c30e4
JokerSiyuan <1361118696@qq.com>
2020-07-28 10:57:56 +0800

    说明

---
+++ Student_achievement_system/project_create.sh
@@ -0,0 +1,16 @@
+#程序入口
+touch main.c
+#系统组成
+touch student.c
+touch student.h
+touch teacher.c
+touch teacher.h
+touch principal.c
+touch principal.h
+#工具函数
+touch tools.c
+touch tools.h
+#编译脚本
+ls > Makefile
+#编写脚本
+ls > Student_achievement_system.conf
